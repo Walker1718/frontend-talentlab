e0030b7f801f4c7d7d5193405337c763baab73b4
Walker1718 <carlos.aip212@gmail.com>
2023-04-24 11:12:22 -0400

    HOTFix: Unit Test sale.component

--- src/app/components/sales/sale/sale.component.spec.ts
+++ src/app/components/sales/sale/sale.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SaleComponent } from './sale.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('SaleComponent', () => {
   let component: SaleComponent;
@@ -8,6 +11,7 @@ describe('SaleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule, FormsModule, ReactiveFormsModule],
       declarations: [ SaleComponent ]
     })
     .compileComponents();
